8ce41e749f3532a21dc59d7e8496df7bb5d3f3cd
sunxun <sunxun@ltidc.cn>
2023-09-12 10:58:23 +0800

    tangshi

---
+++ data/poetry/get_data.py
@@ -0,0 +1,16 @@
+# _*_ condign:utf-8
+
+import glob
+import json
+
+datas = glob.glob("E:/project/python/chinese-poetry/全唐诗/poet.*.json")
+
+for data in datas:
+    with open(data, 'r', encoding='utf-8') as fp:
+        item = json.load(fp)
+        for each in item :
+            if len(each['paragraphs']) == 2 and len(each['paragraphs'][0]) == 12 and len(each['paragraphs'][1]) == 12:
+                with open("tangshi.txt", "a", encoding='utf-8') as f:
+                    f.write("".join(each['paragraphs']))
+                    f.write('\n')
+
